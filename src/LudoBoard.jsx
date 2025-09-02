7c067f1a856057903d558bb99de626b7aa691c7e
Rajshekar badiger <rajshekarbadiger10@gmail.com>
2025-09-02 23:33:00 +0530

    oject and array with states

---
+++ src/LudoBoard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function LudoBoard(){
+    let [Moves,setMoves] = useState({blue:0,yellow:0,green:0,red:0});
+    let [arr,setarr] = useState(["no moves"]);
+
+
+
+    let updateBlue = () => {
+        setMoves((prevvalue) => {
+            return { ...prevvalue, blue : prevvalue.blue +1}
+        });
+
+        setarr([...arr,"blue moved"]);
+    }
+    let updateYellow = () => {
+        setMoves({...Moves, yellow: Moves.yellow +1});
+    }
+    return(
+        <div>
+            <p>game begin</p>
+            <div className="board">
+                <p>{arr}</p>
+                <p>Blue moves = {Moves.blue} </p>
+                <button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1</button>
+                <p>yellow moves = {Moves.yellow} </p>
+                <button style={{backgroundColor:"yellow"}} onClick={updateYellow}>+1</button>
+                <p>Green moves = {Moves.green}</p>
+                <button style={{backgroundColor:"green"}}>+1</button>
+                <p>Red  moves = {Moves.red}</p>
+                <button style={{backgroundColor:"red"}}>+1</button>
+            </div>
+        </div>
+    );
+}
